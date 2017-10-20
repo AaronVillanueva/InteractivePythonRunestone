1c73b1899afebced933616950db2d83c60fc57ff
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2017-10-20 08:19:54 -0500

    Create Numeros Primos.py

---
+++ Capitulo 7/Numeros Primos.py
@@ -0,0 +1,13 @@
+def numeroPrimo(n):
+    if n%2==0 or n%3==0 or n%5==0:
+        conclusion=False
+    else:
+        conclusion=True
+    return(conclusion)
+
+def Main():
+    x=int(input("Ingrese un número entero: "))
+    final=numeroPrimo(x)
+    print(final)
+
+Main()
